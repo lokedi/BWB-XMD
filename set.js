e921384e0e3eee724194bf63914c6111070f180f
lokedi <gregorykemboi496@gmail.com>
2025-06-27 07:42:22 +0300

    Update set.js

--- set.js
+++ set.js
@@ -11,7 +11,7 @@ const DATABASE_URL = process.env.DATABASE_URL === undefined
 module.exports = { session: process.env.SESSION_ID || '',
     PREFIXE: process.env.PREFIX || ".",
     OWNER_NAME: process.env.OWNER_NAME || "princetech",
-    NUMERO_OWNER : process.env.NUMERO_OWNER || "255614545735",              
+    NUMERO_OWNER : process.env.NUMERO_OWNER || "254720874911",              
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'no',
     BOT : process.env.BOT_NAME || 'BWB-XMD',
